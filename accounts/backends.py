07cbdaeb61b55b15dfc8546733775780b81c51e0
nimamze <nimamze3@gmail.com>
2026-04-19 21:34:15 +0330

    add validation for custom authentication

--- accounts/backends.py
+++ accounts/backends.py
@@ -6,10 +6,13 @@ User = get_user_model()
 
 class CustomAuthentication(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
+        phone = username or kwargs.get("phone")
+        if phone is None or password is None:
+            return None
         try:
-            user = User.objects.get(phone=username)
+            user = User.objects.get(phone=phone)
         except User.DoesNotExist:
             return None
-        if user.check_password(password) and self.user_can_authenticate(user):  # type: ignore
+        if user.check_password(password) and self.user_can_authenticate(user):
             return user
         return None
