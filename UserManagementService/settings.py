6b49c434492a475404e3b908c374b2773beec581
nimamze <nimamze3@gmail.com>
2026-04-19 16:48:05 +0330

    add custom authentication

--- UserManagementService/settings.py
+++ UserManagementService/settings.py
@@ -95,3 +95,8 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+AUTHENTICATION_BACKENDS = [
+    "accounts.backend.CustomAuthentication",
+    "django.contrib.auth.backends.ModelBackend",
+]
