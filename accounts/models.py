ba7ae52213abf7293382a658066d720172cf57ef
nimamze <nimamze3@gmail.com>
2026-04-19 21:35:00 +0330

    add validation and delete unneccary codes in accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,18 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from core.models import SoftDeleteManager, SoftDeleteModel
 from django.db import models
+from django.contrib.auth.password_validation import validate_password
 
 
 class UserManager(BaseUserManager, SoftDeleteManager):
     def create_user(self, email, phone, password, **kwargs):
+        if not phone:
+            raise ValueError("Phone is required")
+        if not password:
+            raise ValueError("Password is required")
         email = self.normalize_email(email)
         user = self.model(email=email, phone=phone, **kwargs)
+        validate_password(password, user)
         user.set_password(password)
         user.save()
         return user
@@ -25,14 +31,13 @@ class UserManager(BaseUserManager, SoftDeleteManager):
 
 class CustomUser(SoftDeleteModel, AbstractUser):
     email = models.EmailField(unique=True)
-    phone = models.CharField(unique=True, max_length=16)
+    phone = models.CharField(unique=True, max_length=16, db_index=True)
     image = models.ImageField(upload_to="users/avatars/", null=True, blank=True)
     is_seller = models.BooleanField(default=False)
     username = None
     USERNAME_FIELD = "phone"
     REQUIRED_FIELDS = ["email", "first_name", "last_name"]
     objects = UserManager()
-    all_objects = models.Manager()
 
     def __str__(self):
         return f"{self.get_full_name()}"
